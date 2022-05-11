0b0a83262125702a04d24d9727f612b2fc5e668f
yk-jp <k82.y12@gmail.com>
2022-05-10 21:52:42 -0700

    add neccessary UI Item

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var billAmountTextField: UITextField!
+    @IBOutlet var tipAmountLabel: UILabel!
+    @IBOutlet var tipPercentageTextField: UILabel!
+
+    @IBAction func adjustTipPercentage(_ sender: UISlider) {
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
