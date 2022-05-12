963750e9a70469657e5dabc807a60abfccede0f7
yk-jp <k82.y12@gmail.com>
2022-05-11 23:10:31 -0700

    complete logic.
    a remaining task is to apply user-friendly keyboard

---
+++ TipCalculator/TipCalculator/Tips.swift
@@ -0,0 +1,29 @@
+//
+//  Tips.swift
+//  TipCalculator
+//
+//  Created by Yusuke K on 2022-05-11.
+//
+
+import Foundation
+
+struct Tips {
+    var tipPercentage:Double
+    var val: Double
+    var totalAmount:Double {
+       return val * (1 + tipPercentage)
+    }
+    
+    init() {
+        tipPercentage = 0.0
+        val = 0.0
+    }
+    
+    mutating func updatePercentage(percentage newVal:Double) {
+        tipPercentage = newVal
+    }
+    
+    mutating func updateVal(val newVal:Double) {
+        val = newVal
+    }
+}
--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -7,22 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var tips: Tips = Tips()
+    
     @IBOutlet var billAmountTextField: UITextField!
     @IBOutlet var tipAmountLabel: UILabel!
     @IBOutlet var tipPercentageTextField: UILabel!
-
+    
     @IBAction func adjustTipPercentage(_ sender: UISlider) {
-        
-        
+        let newPercentage = sender.value
+        tips.updatePercentage(percentage: Double(newPercentage))
+        updateLayout()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // hook up billAmountTextField to the view controller
+//        billAmountTextField.delegate = self
+        updateLayout()
     }
-
-
+    
+    
+    @IBAction func calculateTips(_ sender: UIButton) {
+        updateValue()
+        updateLayout()
+    }
+    
+    // when return key is tapped
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        updateValue()
+//        updateLayout()
+//        textField.resignFirstResponder()
+//        return true
+//    }
+    
+    // dismissing keyboard by touching outside of keyboard
+    // it is reacted even when keyboard is not shown up
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func updateValue() {
+        guard billAmountTextField.text != nil else {
+            return
+        }
+        
+        if let billAmountText = billAmountTextField.text {
+            let newVal = Double(billAmountText)
+            tips.updateVal(val: newVal!)
+        }
+    }
+    
+    func updateLayout() {
+        tipAmountLabel.text = String(format: "$%.2f", tips.totalAmount)
+        tipPercentageTextField.text = String(format:"$%.2f", tips.tipPercentage) + " %" //e.g 0.21
+    }
 }
 
